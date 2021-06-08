e93adda9d6953aa934e84e8e70118daa8fb2c8b2
Ethan-Edmond <eedmond@uccs.edu>
2021-06-08 13:49:26 -0600

    Connected up delete

--- src/components/MovieHeader.js
+++ src/components/MovieHeader.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from "react-redux";
 
-const MovieHeader = (props) => {
-    const appTitle = "";
+const MovieHeader = ({appTitle}) => {
     const displayFavorites = true;
-    
+
     return(<div className="table-title">
         <div className="row">
         <div className="col-sm-6">
@@ -17,6 +17,10 @@ const MovieHeader = (props) => {
         </div>
         </div>
     </div>);
-}
+};
 
-export default MovieHeader;
+const stateToProps = (state) => ({
+  appTitle: state.appTitle
+});
+
+export default connect(stateToProps)(MovieHeader);
--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -1,5 +1,5 @@
 import { ADD_MOVIE, DELETE_MOVIE } from '../actions/movieActions.js';
-import movies from './../data.js';
+import movies from '../data.js';
 
 const initialState = {
     movies: movies,
@@ -7,14 +7,15 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case DELETE_MOVIE:
-            return {
-                movies: state.movies.filter(item=>(action.payload !== item.id))
-            };
-        default:
-            return state;
-    }
+  switch(action.type) {
+  case DELETE_MOVIE:
+    return {
+      ...state,
+      movies: state.movies.filter(item=>(action.payload !== item.id))
+    };
+  default:
+    return state;
+  }
 }
 
 export default reducer;
